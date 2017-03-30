f6eb2fbe467ca72a24f582a5eebffbc7420e3344
Kevin <kevin.ditraglia@gmail.com>
2017-03-29 22:27:12 -0400

    fix bug in pathfinding, all enemies were sharing the same list and popping from it

--- TowerDefense/GameEngine.cs
+++ TowerDefense/GameEngine.cs
@@ -90,21 +90,21 @@ namespace TowerDefense
             {
                 for (int i = 0; i < (15 + GameStats.Level); i++)
                 {
-                    enemylist.Add(new Enemy(GameStats.Level * 8, 2, ResourceManager.Enemy, bestPath, "Malaria", 1.0f, .4f));
+                    enemylist.Add(new Enemy(GameStats.Level * 8, 2, ResourceManager.Enemy, new List<Node>(bestPath), "Malaria", 1.0f, .4f));
                 }
             }
             else if (num < .6)
             {
                 for (int i = 0; i < (30 + 2 * GameStats.Level); i++)
                 {
-                    enemylist.Add(new Enemy(GameStats.Level * 4, 2, ResourceManager.Enemy, bestPath, "Tuberculosis", .33f, .2f));
+                    enemylist.Add(new Enemy(GameStats.Level * 4, 2, ResourceManager.Enemy, new List<Node>(bestPath), "Tuberculosis", .33f, .2f));
                 }
             }
             else
             {
                 for (int i = 0; i < (5 + GameStats.Level / 2); i++)
                 {
-                    enemylist.Add(new Enemy(GameStats.Level * 16, 1, ResourceManager.Enemy, bestPath, "AIDS", 1.5f, .8f));
+                    enemylist.Add(new Enemy(GameStats.Level * 16, 1, ResourceManager.Enemy, new List<Node>(bestPath), "AIDS", 1.5f, .8f));
                 }
             }
         }
