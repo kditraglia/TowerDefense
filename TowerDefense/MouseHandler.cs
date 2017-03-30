e7461738c5dd9337adbd25b0cc74109764d5a5e0
Kevin <kevin.ditraglia@gmail.com>
2017-03-29 23:15:31 -0400

    cleanup the last of the mouse stuff

--- TowerDefense/MouseHandler.cs
+++ TowerDefense/MouseHandler.cs
@@ -51,11 +51,169 @@ namespace TowerDefense
             this.pos = pos;
             this.tex = tex;
         }
-        public void Update()
+        public void Update(GameEngine gameEngine, GameMap gameMap)
         {
             MouseState = Mouse.GetState();
             pos.X = MouseState.X;
             pos.Y = MouseState.Y;
+
+            if (GameStats.PlayerLoses)
+            {
+                return;
+            }
+
+            if (!GameStats.AttackPhase)
+            {
+                if (MouseState.LeftButton == ButtonState.Pressed)
+                {
+                    //I took this out of the HandleLeftClick method, since it's the only thing I want to allow holding the button down
+                    //It doesn't feel right to not be able to drag the mouse when making walls
+                    if (SelectionContext == SelectionContext.PlacingWall && HoveringContext == HoveringContext.EmptyNode)
+                    {
+                        Node n = HoveredObject as Node;
+                        if (!gameMap.CheckForPath(n.simplePos.X, n.simplePos.Y, this, CheckForPathType.TogglingWall))
+                        {
+                            MessageLog.IllegalPosition();
+                        }
+                        else if (GameStats.Gold >= 1)
+                        {
+                            n.wall = true;
+                            n.UpdateTex(tex);
+                            GameStats.Gold = GameStats.Gold - 1;
+                            ResourceManager.WallSound.Play();
+                        }
+                        else
+                        {
+                            MessageLog.NotEnoughGold();
+                        }
+                    }
+                }
+                if (MouseState.LeftButton == ButtonState.Pressed && !MouseClicked)
+                {
+                    HandleLeftClick(gameEngine, gameMap);
+                    MouseClicked = true;
+                }
+                if (MouseState.RightButton == ButtonState.Pressed && !MouseClicked)
+                {
+                    HandleRightClick(gameEngine, gameMap);
+                    MouseClicked = true;
+                }
+                if (MouseState.LeftButton == ButtonState.Released && MouseState.RightButton == ButtonState.Released)
+                {
+                    MouseClicked = false;
+                }
+            }
+
+            HoveringContext = HoveringContext.None;
+            HoveredObject = null;
+        }
+
+        private void HandleLeftClick(GameEngine gameEngine, GameMap gameMap)
+        {
+            if (HoveredObject != null)
+            {
+                HoveredObject.HandleLeftClick(this);
+            }
+            if (HoveringContext == HoveringContext.ButtonStart && !GameStats.AttackPhase && SelectionContext == SelectionContext.None)
+            {
+                UpdateTex(ResourceManager.DefaultCursor);
+                gameEngine.StartLevel(gameMap.GetBestPath());
+            }
+            gameEngine.HandleLeftClick(this);
+
+            if (SelectionContext == SelectionContext.PlacingPortalEntrance && HoveringContext == HoveringContext.EmptyNode)
+            {
+                Node n = HoveredObject as Node;
+                n.portal = true;
+                n.UpdateTex(tex);
+                PortalEntrance = n;
+                SelectionContext = SelectionContext.PlacingPortalExit;
+            }
+            else if (SelectionContext == SelectionContext.PlacingPortalExit && HoveringContext == HoveringContext.EmptyNode)
+            {
+                Node portalExit = HoveredObject as Node;
+                Node portalEntrance = PortalEntrance;
+                if (!gameMap.CheckForPath(portalExit.simplePos.X, portalExit.simplePos.Y, this, CheckForPathType.AddingPortal))
+                {
+                    MessageLog.IllegalPosition();
+                }
+                else if (GameStats.Gold >= 20)
+                {
+                    portalExit.portal = true;
+                    portalExit.UpdateTex(tex);
+                    portalExit.portalsTo = portalEntrance;
+                    portalEntrance.portalsTo = portalExit;
+                    SelectionContext = SelectionContext.PlacingPortalEntrance;
+                    GameStats.Gold = GameStats.Gold - 20;
+                }
+                else
+                {
+                    MessageLog.NotEnoughGold();
+                }
+            }
+            else if (SelectionContext == SelectionContext.PlacingCheese && HoveringContext == HoveringContext.EmptyNode)
+            {
+                Node n = HoveredObject as Node;
+                if (!gameMap.CheckForPath(n.simplePos.X, n.simplePos.Y, this, CheckForPathType.TogglingCheese))
+                {
+                    MessageLog.IllegalPosition();
+                }
+                else if (GameStats.Gold >= 20)
+                {
+                    n.cheese = true;
+                    n.UpdateTex(tex);
+                    GameStats.Gold = GameStats.Gold - 20;
+                    ResourceManager.WallSound.Play();
+                }
+                else
+                {
+                    MessageLog.NotEnoughGold();
+                }
+            }
+        }
+
+        private void HandleRightClick(GameEngine gameEngine, GameMap gameMap)
+        {
+            gameEngine.HandleRightClick(this);
+
+            if (SelectionContext == SelectionContext.PlacingPortalExit)
+            {
+                PortalEntrance.portal = false;
+                PortalEntrance.defaultSet();
+                PortalEntrance = null;
+            }
+            else if (HoveringContext == HoveringContext.FilledNode && SelectionContext == SelectionContext.None)
+            {
+                Node n = HoveredObject as Node;
+                if (n.wall && gameMap.CheckForPath(n.simplePos.X, n.simplePos.Y, this, CheckForPathType.TogglingWall))
+                {
+                    GameStats.Gold = GameStats.Gold + 1;
+                    n.wall = false;
+                    n.defaultSet();
+                    ResourceManager.SellSound.Play();
+                }
+                else if (n.portal && gameMap.CheckForPath(n.simplePos.X, n.simplePos.Y, this, CheckForPathType.RemovingPortal))
+                {
+                    n.portal = false;
+                    n.defaultSet();
+                    n.portalsTo.portal = false;
+                    n.portalsTo.portalsTo = null;
+                    n.portalsTo.defaultSet();
+                    n.portalsTo = null;
+                    GameStats.Gold = GameStats.Gold + 20;
+                    ResourceManager.SellSound.Play();
+                }
+                else if (n.cheese && gameMap.CheckForPath(n.simplePos.X, n.simplePos.Y, this, CheckForPathType.TogglingCheese))
+                {
+                    GameStats.Gold = GameStats.Gold + 20;
+                    n.cheese = false;
+                    n.defaultSet();
+                    ResourceManager.SellSound.Play();
+                }
+            }
+
+            UpdateTex(ResourceManager.DefaultCursor);
+            SelectionContext = SelectionContext.None;
         }
 
         internal bool MouseInGameBounds()
--- TowerDefense/TowerDefense.cs
+++ TowerDefense/TowerDefense.cs
@@ -49,8 +49,11 @@ namespace TowerDefense
 
         protected override void Update(GameTime gameTime)
         {
-            HandleMouse();
-            mouse.Update();
+            mouse.Update(gameEngine, gameMap);
+            gameEngine.HandleMouseHover(mouse);
+            gameMap.HandleMouseHover(mouse);
+            gameHUD.HandleMouseHover(mouse);
+
             gameEngine.Update(gameTime);
             base.Update(gameTime);
         }
@@ -69,174 +72,6 @@ namespace TowerDefense
             batch.End();
 
             base.Draw(gameTime);
-        }
-
-        private void HandleMouse()
-        {
-            if (GameStats.PlayerLoses)
-            {
-                return;
-            }
-            HandleMouseHover();
-            if (!GameStats.AttackPhase)
-            {
-                if (mouse.MouseState.LeftButton == ButtonState.Pressed)
-                {
-                    //I took this out of the HandleLeftClick method, since it's the only thing I want to allow holding the button down
-                    //It doesn't feel right to not be able to drag the mouse when making walls
-                    if (mouse.SelectionContext == SelectionContext.PlacingWall && mouse.HoveringContext == HoveringContext.EmptyNode)
-                    {
-                        Node n = mouse.HoveredObject as Node;
-                        if (!gameMap.CheckForPath(n.simplePos.X, n.simplePos.Y, mouse, CheckForPathType.TogglingWall))
-                        {
-                            MessageLog.IllegalPosition();
-                        }
-                        else if (GameStats.Gold >= 1)
-                        {
-                            n.wall = true;
-                            n.UpdateTex(mouse.tex);
-                            GameStats.Gold = GameStats.Gold - 1;
-                            ResourceManager.WallSound.Play();
-                        }
-                        else
-                        {
-                            MessageLog.NotEnoughGold();
-                        }
-                    }
-                }
-                if (mouse.MouseState.LeftButton == ButtonState.Pressed && !mouse.MouseClicked)
-                {
-                    HandleLeftClick();
-                    mouse.MouseClicked = true;
-                }
-                if (mouse.MouseState.RightButton == ButtonState.Pressed && !mouse.MouseClicked)
-                {
-                    HandleRightClick();
-                    mouse.MouseClicked = true;
-                }
-                if (mouse.MouseState.LeftButton == ButtonState.Released && mouse.MouseState.RightButton == ButtonState.Released)
-                {
-                    mouse.MouseClicked = false;
-                }
-            }
-        }
-
-        private void HandleMouseHover()
-        {
-            mouse.HoveringContext = HoveringContext.None;
-            mouse.HoveredObject = null;
-
-            gameEngine.HandleMouseHover(mouse);
-            gameMap.HandleMouseHover(mouse);
-            gameHUD.HandleMouseHover(mouse);
-        }
-
-        private void HandleLeftClick()
-        {
-            if (mouse.HoveredObject != null)
-            {
-                mouse.HoveredObject.HandleLeftClick(mouse);
-            }
-            if (mouse.HoveringContext == HoveringContext.ButtonStart && !GameStats.AttackPhase && mouse.SelectionContext == SelectionContext.None)
-            {
-                mouse.UpdateTex(ResourceManager.DefaultCursor);
-                gameEngine.StartLevel(gameMap.GetBestPath());
-            }
-            gameEngine.HandleLeftClick(mouse);
-
-            if (mouse.SelectionContext == SelectionContext.PlacingPortalEntrance && mouse.HoveringContext == HoveringContext.EmptyNode)
-            {
-                Node n = mouse.HoveredObject as Node;
-                n.portal = true;
-                n.UpdateTex(mouse.tex);
-                mouse.PortalEntrance = n;
-                mouse.SelectionContext = SelectionContext.PlacingPortalExit;
-            }
-            else if (mouse.SelectionContext == SelectionContext.PlacingPortalExit && mouse.HoveringContext == HoveringContext.EmptyNode)
-            {
-                Node portalExit = mouse.HoveredObject as Node;
-                Node portalEntrance = mouse.PortalEntrance;
-                if (!gameMap.CheckForPath(portalExit.simplePos.X, portalExit.simplePos.Y, mouse, CheckForPathType.AddingPortal))
-                {
-                    MessageLog.IllegalPosition();
-                }
-                else if (GameStats.Gold >= 20)
-                {
-                    portalExit.portal = true;
-                    portalExit.UpdateTex(mouse.tex);
-                    portalExit.portalsTo = portalEntrance;
-                    portalEntrance.portalsTo = portalExit;
-                    mouse.SelectionContext = SelectionContext.PlacingPortalEntrance;
-                    GameStats.Gold = GameStats.Gold - 20;
-                }
-                else
-                {
-                    MessageLog.NotEnoughGold();
-                }
-            }
-            else if (mouse.SelectionContext == SelectionContext.PlacingCheese && mouse.HoveringContext == HoveringContext.EmptyNode)
-            {
-                Node n = mouse.HoveredObject as Node;
-                if (!gameMap.CheckForPath(n.simplePos.X, n.simplePos.Y, mouse, CheckForPathType.TogglingCheese))
-                {
-                    MessageLog.IllegalPosition();
-                }
-                else if (GameStats.Gold >= 20)
-                {
-                    n.cheese = true;
-                    n.UpdateTex(mouse.tex);
-                    GameStats.Gold = GameStats.Gold - 20;
-                    ResourceManager.WallSound.Play();
-                }
-                else
-                {
-                    MessageLog.NotEnoughGold();
-                }
-            }
-        }
-
-        private void HandleRightClick()
-        {
-            gameEngine.HandleRightClick(mouse);
-
-            if (mouse.SelectionContext == SelectionContext.PlacingPortalExit)
-            {
-                mouse.PortalEntrance.portal = false;
-                mouse.PortalEntrance.defaultSet();
-                mouse.PortalEntrance = null;
-            }
-            else if (mouse.HoveringContext == HoveringContext.FilledNode && mouse.SelectionContext == SelectionContext.None)
-            {
-                Node n = mouse.HoveredObject as Node;
-                if (n.wall && gameMap.CheckForPath(n.simplePos.X, n.simplePos.Y, mouse, CheckForPathType.TogglingWall))
-                {
-                    GameStats.Gold = GameStats.Gold + 1;
-                    n.wall = false;
-                    n.defaultSet();
-                    ResourceManager.SellSound.Play();
-                }
-                else if (n.portal && gameMap.CheckForPath(n.simplePos.X, n.simplePos.Y, mouse, CheckForPathType.RemovingPortal))
-                {
-                    n.portal = false;
-                    n.defaultSet();
-                    n.portalsTo.portal = false;
-                    n.portalsTo.portalsTo = null;
-                    n.portalsTo.defaultSet();
-                    n.portalsTo = null;
-                    GameStats.Gold = GameStats.Gold + 20;
-                    ResourceManager.SellSound.Play();
-                }
-                else if (n.cheese && gameMap.CheckForPath(n.simplePos.X, n.simplePos.Y, mouse, CheckForPathType.TogglingCheese))
-                {
-                    GameStats.Gold = GameStats.Gold + 20;
-                    n.cheese = false;
-                    n.defaultSet();
-                    ResourceManager.SellSound.Play();
-                }
-            }
-
-            mouse.UpdateTex(ResourceManager.DefaultCursor);
-            mouse.SelectionContext = SelectionContext.None;
         }
     }
 }
