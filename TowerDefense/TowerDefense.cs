df2110138640b9a23bcb25bf9d66185b780d9058
Kevin <kevin.ditraglia@gmail.com>
2017-04-17 18:24:10 -0400

    change scaling code to not need to be calculated on draw

--- TowerDefense/TowerDefense.cs
+++ TowerDefense/TowerDefense.cs
@@ -10,6 +10,7 @@ namespace TowerDefense
 {
     public class TowerDefense : Game
     {
+        Vector2 gameScale;
         GraphicsDeviceManager graphics;
         SpriteBatch batch;
 
@@ -46,10 +47,8 @@ namespace TowerDefense
 
         protected override void Update(GameTime gameTime)
         {
-            float scaleX = (float)graphics.GraphicsDevice.Viewport.Width / Constants.GameSize.X;
-            float scaleY = (float)graphics.GraphicsDevice.Viewport.Height / Constants.GameSize.Y;
-
-            inputHandler.Update(gameTime, new Vector2(scaleX, scaleY));
+            gameScale = new Vector2((float)graphics.GraphicsDevice.Viewport.Width / Constants.GameSize.X, (float)graphics.GraphicsDevice.Viewport.Height / Constants.GameSize.Y);
+            inputHandler.Update(gameTime, gameScale);
             gameMap.Update(gameTime, inputHandler);
             gameHUD.Update(gameTime, inputHandler);
             gameEngine.Update(gameTime, inputHandler);
@@ -59,11 +58,7 @@ namespace TowerDefense
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.WhiteSmoke);
-            float scaleX = (float)graphics.GraphicsDevice.Viewport.Width / Constants.GameSize.X;
-            float scaleY = (float)graphics.GraphicsDevice.Viewport.Height / Constants.GameSize.Y;
-            Matrix scaleMatrix = Matrix.CreateScale(scaleX, scaleY, 1.0f);
-
-            batch.Begin(transformMatrix: scaleMatrix);
+            batch.Begin(transformMatrix: Matrix.CreateScale(new Vector3(gameScale, 1.0f)));
 
             inputHandler.Draw(batch);
             gameMap.Draw(batch);
